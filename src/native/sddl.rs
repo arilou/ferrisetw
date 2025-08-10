30a494ce4d716b5fccbfd0fa73074d21b030dd62
Elad Solomon <elad.solomon@wiz.io>
2025-08-10 15:48:14 +0300

    Upgrade windows dep to 0.58
    Signed-off-by: Elad Solomon <elad.solomon@wiz.io>

--- src/native/sddl.rs
+++ src/native/sddl.rs
@@ -1,8 +1,9 @@
 use core::ffi::c_void;
 use std::str::Utf8Error;
 use windows::core::PSTR;
-use windows::Win32::Foundation::{LocalFree, HLOCAL, PSID};
+use windows::Win32::Foundation::{LocalFree, HLOCAL};
 use windows::Win32::Security::Authorization::ConvertSidToStringSidA;
+use windows::Win32::Security::PSID;
 
 /// SDDL native error
 #[derive(Debug)]
